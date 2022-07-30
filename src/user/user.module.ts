ce747c48924f30c7bdbcb4c750a85a9f9bb8417c
이서진 <lsj1222@hanyang.ac.kr>
2022-07-30 22:33:10 +0900

    initial common folder setting

---
+++ src/user/user.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entity/User.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+        UserEntity,
+    ])
+  ],
+  controllers: [],
+  providers: [],
+})
+
+export class UserModule {}
+
+/**
+ * 
+ */
