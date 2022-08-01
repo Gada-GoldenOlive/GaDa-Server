0447420f0aeefee6c304c5929ed9ea5b61290128
이서진 <lsj1222@hanyang.ac.kr>
2022-08-01 14:18:54 +0900

    수정 및 DB 연결
    1. package.json에 오류 있어서 해결함
    - 이름 다르게 설정되어 있어서 GaDa-Server로 변경해둠
    - 그리고 npm install 다시 했음
    2. app.module.ts 수정
    - DB 연결 안 돼 있어서 TypeOrmModule 써서 DB 연결해줌
    - 이거 나중에? 아니면 바로라도 .env 파일 써서 하는 게 좋을 것 같은데
      어떻게 생각하는지!!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserEntity } from './user/entity/User.entity';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'tjwlsWkd!123',
+      database: 'gada',
+      synchronize: true,
+      entities: [
+        UserEntity,
+      ],
+    }),
     UserModule,
   ],
   controllers: [AppController],
