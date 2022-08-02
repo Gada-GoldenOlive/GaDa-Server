3a04418ff3f3dc280a33b63c7078472382777480
SeoSiun <siun0816@hanyang.ac.kr>
2022-08-02 22:59:01 +0900

    환경변수 설정 (.env)
    1. app.module.ts에서ConfigModule을 사용해 DB정보를 .env에서 가져오도록 함.
    2. .gitignore에 .env 추가
    3. 아래 패키지 설치함.
     - npm i --save @nestjs/config
     - npm i cross-env
     - npm i joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserEntity } from './user/entity/User.entity';
@@ -7,13 +9,25 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+        envFilePath: '.env',
+        isGlobal: true,
+        validationSchema: Joi.object({
+            NODE_ENV: Joi.string().valid('production'),
+            DB_HOST: Joi.string().required(),
+            DB_PORT: Joi.string().required(),
+            DB_USER: Joi.string().required(),
+            DB_PASSWORD: Joi.string().required(),
+            DB_NAME: Joi.string().required(),
+        }),
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'tjwlsWkd!123',
-      database: 'gada',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       entities: [
         UserEntity,
