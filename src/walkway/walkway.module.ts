31371a05568abe90b0b143b1c5fd678b6faa30ed
SeoSiun <siun0816@hanyang.ac.kr>
2022-08-06 19:50:57 +0900

    walkway.module.ts 수정
    - import 정리
    - controller에 WalkController 추가

--- src/walkway/walkway.module.ts
+++ src/walkway/walkway.module.ts
@@ -2,18 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { WalkwayController } from './controller/WalkwayController';
+import { WalkController } from './controller/WalkController';
 import { WalkEntity } from './entity/Walk.entity';
 import { WalkwayEntity } from './entity/Walkway.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-        WalkwayEntity,
-        WalkEntity,
-    ])
-  ],
-  controllers: [WalkwayController],
-  providers: [],
+    imports: [
+        TypeOrmModule.forFeature([
+            WalkwayEntity,
+            WalkEntity,
+        ])
+    ],
+    controllers: [
+        WalkwayController,
+        WalkController
+    ],
+    providers: [],
 })
 
 export class WalkwayModule {}
