6129db8ca34de5b452bb9fdc689f5283e947cead
이서진 <lsj1222@hanyang.ac.kr>
2022-08-10 23:39:32 +0900

    Pin get api 대강 구현
    1. mysql repo 작성
    - findAll() 구현
    - mapper에서 toDomain, toDomains 구현
    
    2. Pin Controller 구현
    - @Get api
    - walkway랑 user find usecase가 아직 완성되지 않아서 완성 후에 다시 수정
    - pin controller dto 수정 (user id도 전달)

--- src/pin/infra/mysql/MysqlPinRepository.ts
+++ src/pin/infra/mysql/MysqlPinRepository.ts
@@ -1,8 +1,10 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Pin } from '../../domain/Pin';
+import { PinStatus } from '../../domain/PinStatus';
 import { PinEntity } from '../../entity/Pin.entity';
 import { IPinRepository } from '../IPinRepository';
+import { MysqlPinRepositoryMapper } from './mapper/MysqlPinRepositoryMapper';
 
 export class MysqlPinRepository implements IPinRepository {
     constructor(
@@ -18,7 +20,20 @@ export class MysqlPinRepository implements IPinRepository {
         throw new Error('Method not implemented.');
     }
 
-    findAll(walkwayId: string): Promise<Pin[]> {
-        throw new Error('Method not implemented.');
+    async findAll(walkwayId: string): Promise<Pin[]> {
+        const foundPins = await this.pinRepository.find({
+            where: {
+                status: PinStatus.NORMAL,
+                walkway: {
+                    id: walkwayId,
+                },
+            },
+            relations: ['walkway'],
+            order: {
+                createdAt: 'DESC',
+            },
+        });
+
+        return MysqlPinRepositoryMapper.toDomains(foundPins)
     }
 }
