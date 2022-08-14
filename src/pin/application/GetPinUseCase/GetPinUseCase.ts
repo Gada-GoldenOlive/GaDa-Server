4d6273e32bb0f8630c820a971648786557b069b6
이서진 <lsj1222@hanyang.ac.kr>
2022-08-14 21:17:37 +0900

    Pin Get Api 구현
        - get pin usecase 작성, usecase dto 생성
        - 일단 직접적인 pin id로만 찾는 거 생각했는데 다른 경우 없겠지?
        - mysql에서 pin findOne 구현 (이거 나중에 get이나 find로 이름 통일시켜야 할듯..!)
        - module에 추가해줌

---
+++ src/pin/application/GetPinUseCase/GetPinUseCase.ts
@@ -0,0 +1,40 @@
+import { Inject } from '@nestjs/common';
+
+import { UseCase } from '../../../common/application/UseCase';
+import { IPinRepository, PIN_REPOSITORY } from '../../infra/IPinRepository';
+import { IGetPinUseCaseRequest } from './dto/IGetPinUseCaseRequest';
+import { IGetPinUseCaseResponse } from './dto/IGetPinUseCaseResponse';
+
+export enum GetPinUseCaseCodes {
+	SUCCESS = 'SUCCESS',
+	FAILURE = 'FAILURE',
+	NO_PIN_FOUND_ERROR = 'Corresponding pin does not exist.',
+}
+
+export class GetPinUseCase implements UseCase<IGetPinUseCaseRequest, IGetPinUseCaseResponse> {
+	constructor(
+		@Inject(PIN_REPOSITORY)
+		private readonly pinRepository: IPinRepository,
+	) {}
+
+	async execute(request: IGetPinUseCaseRequest): Promise<IGetPinUseCaseResponse> {
+		try {
+			const pin = await this.pinRepository.findOne(request.id);
+
+			if (!pin) {
+				return {
+					code: GetPinUseCaseCodes.NO_PIN_FOUND_ERROR,
+				}
+			}
+
+			return {
+				code: GetPinUseCaseCodes.SUCCESS,
+				pin,
+			}
+		} catch (e) {
+			return {
+				code: GetPinUseCaseCodes.FAILURE,
+			}
+		}
+	}
+}
