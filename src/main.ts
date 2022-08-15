9cb555e8df8410fa81e0e2edc248f606f90cd578
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2022-08-15 15:43:44 +0900

    image size error 해결: body-parser로 limit 풀어줌. (#49)
    - body-parser, express 설치

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { urlencoded, json } from 'body-parser';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.static(join(process.cwd(), '../dist')));
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
+  app.enableCors();
+
   const options = new DocumentBuilder()
   .setTitle('GaDa API LIST')
   .setDescription('GaDa의 API LIST입니다.')
