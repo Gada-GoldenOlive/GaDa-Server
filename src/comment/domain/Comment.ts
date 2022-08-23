5c915e38fa6a848e60652c03be7d22d0204436fb
SeoSiun <siun0816@hanyang.ac.kr>
2022-08-23 14:48:01 +0900

    comment domain 완성
    1.CommentContent, CommentStatus, Comment 도메인 완성
    
    2. content는 empty검사 하면 되는거 맞겠져..?
    
    3. status도 만들었슴돠

---
+++ src/comment/domain/Comment.ts
@@ -0,0 +1,78 @@
+import _ from "lodash";
+import { AggregateRoot } from "../../common/domain/AggregateRoot";
+import { Result } from "../../common/presentationals/Result";
+
+import { Pin } from "../../pin/domain/Pin";
+import { User } from "../../user/domain/User";
+import { CommentContent } from "./CommentContent";
+import { CommentStatus, COMMENT_STATUS } from "./CommentStatus";
+
+export interface CommentNewProps {
+    content: CommentContent;
+    status?: COMMENT_STATUS;
+    pin: Pin;
+    user: User;
+}
+
+export interface CommentProps extends CommentNewProps {
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export const PROPS_VALUES_ARE_REQUIRED = 'Props values are required.';
+
+export class Comment extends AggregateRoot<CommentProps> {
+    private constructor(props: CommentProps, id?: string) {
+        super(props, id);
+    }
+
+    static createNew(props: CommentNewProps): Result<Comment> {
+        if (_.isNil(props.content) || _.isNil(props.pin) || _.isNil(props.user)) {
+            return Result.fail(PROPS_VALUES_ARE_REQUIRED);
+        }
+
+        return Result.ok(new Comment({
+            ...props,
+            status: this.getCommentStatusAndSetIfStatusIsUndefined(props),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }));
+    }
+
+    static create(props: CommentProps, id: string): Result<Comment> {
+        return Result.ok(new Comment(props, id));
+    }
+
+    get content(): CommentContent {
+        return this.props.content;
+    }
+
+    get status(): COMMENT_STATUS {
+        return this.props.status;
+    }
+    
+    get createdAt(): Date {
+        return this.props.createdAt;
+    }
+
+    get updatedAt(): Date {
+        return this.props.updatedAt;
+    }
+
+    get pin(): Pin {
+        return this.props.pin;
+    }
+
+    get user(): User {
+        return this.props.user;
+    }
+
+    private static getCommentStatusAndSetIfStatusIsUndefined(props: CommentNewProps) {
+        let { status } = props;
+        if (_.isNil(props.status) || _.isEmpty(props.status)) {
+            status = CommentStatus.NORMAL;
+        }
+        
+        return status;
+    }
+}
