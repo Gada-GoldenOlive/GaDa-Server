09d602f87a4839ba65b494b36dc1ca9db07d813e
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2022-09-18 20:41:26 +0900

    record domain, entity 추가 (#82)
    * record domain, entity 추가
    
    - record에 status가 필요한지는 모르겠지만 일단 만들었음.
    - record에 시간, 거리만 있으면 되는거겠지..?
    
    - user entity에도 records 추가해둠
    
    * 리뷰 반영

---
+++ src/user/domain/Record/Record.ts
@@ -0,0 +1,66 @@
+import _ from 'lodash';
+
+import { AggregateRoot } from '../../../common/domain/AggregateRoot';
+import { Result } from '../../../common/presentationals/Result';
+import { User } from '../User/User';
+import { RecordStatus, RECORD_STATUS } from './RecordStatus';
+
+export interface RecordNewProps {
+    status?: RECORD_STATUS;
+    user: User;
+}
+
+export interface RecordProps extends RecordNewProps {
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export const PROPS_VALUES_ARE_REQUIRED = 'Props values are required.';
+
+export class Record extends AggregateRoot<RecordProps> {
+    private constructor(props: RecordProps, id?: string) {
+        super(props, id);
+    }
+
+    static createNew(props: RecordNewProps): Result<Record> {
+        if (_.isNil(props.user)) {
+            return Result.fail(PROPS_VALUES_ARE_REQUIRED);
+        }
+
+        return Result.ok(new Record({
+            ...props,
+            status: this.getRecordStatusAndSetIfStatusIsUndefined(props),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }));
+    }
+
+    static create(props: RecordProps, id: string): Result<Record> {
+        return Result.ok(new Record(props, id));
+    }
+
+    get status(): RECORD_STATUS {
+        return this.props.status;
+    }
+
+    get createdAt(): Date {
+        return this.props.createdAt;
+    }
+
+    get updatedAt(): Date {
+        return this.props.updatedAt;
+    }
+
+    get user(): User {
+        return this.props.user;
+    }
+
+    private static getRecordStatusAndSetIfStatusIsUndefined(props: RecordNewProps) {
+        let { status } = props;
+        if (_.isNil(props.status) || _.isEmpty(props.status)) {
+            status = RecordStatus.NORMAL;
+        }
+
+        return status;
+    }
+}
