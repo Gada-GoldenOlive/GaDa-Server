808f802b0564cab8f8f73f4faf67b60250180c1d
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2022-09-18 20:39:40 +0900

    get all feed에 like 추가 (#80)
    * get all feed에 like 추가
    
    getAllFeed에 like를 추가함.
    review, user에 해당하는 like 존재여부를 리턴하는 함수 만들고,
    getReview에서도 이 함수 호출하는걸로 수정.
    
    * 리뷰 반영

--- src/review/controller/ReviewController.ts
+++ src/review/controller/ReviewController.ts
@@ -13,6 +13,28 @@ import { IGetAllReviewUseCaseResponse } from '../application/GetAllReviewUseCase
 import { GetReviewUseCase, GetReviewUseCaseCodes } from '../application/GetReviewUseCase/IGetReviewUseCase';
 import { GetLikeUseCase, GetLikeUseCaseCodes } from '../application/GetLikeUseCase/IGetLikeUseCase';
 
+const is_like_exist = async (review, userId, getUserUseCase, getLikeUseCase) => {
+    let like = false;
+    if (userId) {
+        const getUserUseCaseResponse = await getUserUseCase.execute({
+            id: userId,
+        });
+
+        if (!getUserUseCaseResponse || getUserUseCaseResponse.code !== GetUserUseCaseCodes.SUCCESS) {
+            throw new HttpException('FAIL TO FIND USER',StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+
+        const getLikeUseCaseResponse = await getLikeUseCase.execute({
+            user: getUserUseCaseResponse.user,
+            review,
+        });
+
+        if (getLikeUseCaseResponse.like)
+            like = true;
+    }
+    return like;
+}
+
 @Controller('reviews')
 @ApiTags('리뷰')
 export class ReviewController {
@@ -139,98 +161,13 @@ export class ReviewController {
 
     }
 
-    @Get('/:reviewId')
-    @ApiOkResponse({
-        type: GetReviewResponse,
-    })
-    @ApiOperation({
-        summary: '개별 리뷰 정보 가져오기',
-        description: '피드>산책로게시물 페이지에 보여질 리뷰 정보 get / userId는 좋아요 여부를 알기 위함. / ' 
-        + 'userId가 주어지지 않으면 like는 false로 리턴'
-    })
-    async getReview(
-        @Param('reviewId') reviewId: string,
-        @Query('userId') userId: string,
-    ): Promise<GetReviewResponse> {
-        const getReviewUseCaseResponse = await this.getReviewUseCase.execute({
-            id: reviewId,
-        });
-        if (getReviewUseCaseResponse.code === GetReviewUseCaseCodes.NO_EXIST_REVIEW)
-            throw new HttpException('NO EXIST REVIEW', StatusCodes.NOT_FOUND);
-
-        if (getReviewUseCaseResponse.code !== GetReviewUseCaseCodes.SUCCESS) {
-            throw new HttpException('FAIL TO FIND REVIEW', StatusCodes.INTERNAL_SERVER_ERROR);
-        }
-
-        let like = false;
-        if (userId) {
-            const getUserUseCaseResponse = await this.getUserUseCase.execute({
-                id: userId,
-            });
-
-            if (!getUserUseCaseResponse || getUserUseCaseResponse.code !== GetUserUseCaseCodes.SUCCESS) {
-                throw new HttpException('FAIL TO FIND USER',StatusCodes.INTERNAL_SERVER_ERROR);
-            }
-
-            const getLikeUseCaseResponse = await this.getLikeUseCase.execute({
-                user: getUserUseCaseResponse.user,
-                review: getReviewUseCaseResponse.review,
-            })
-
-            if (getLikeUseCaseResponse.like)
-                like = true;
-        }
-
-        const review: FeedDto = {
-            review: {
-                id: getReviewUseCaseResponse.review.id,
-                title: getReviewUseCaseResponse.review.title.value,
-                vehicle: getReviewUseCaseResponse.review.vehicle,
-                star: getReviewUseCaseResponse.review.star.value,
-                content: getReviewUseCaseResponse.review.content.value,
-                userImage: getReviewUseCaseResponse.review.walk.user.image.value,
-                userId: getReviewUseCaseResponse.review.walk.user.id,
-                userName: getReviewUseCaseResponse.review.walk.user.name.value,
-                walkwayId: getReviewUseCaseResponse.review.walk.walkway.id,
-                walkwayTitle: getReviewUseCaseResponse.review.walk.walkway.title.value,
-                createdAt: getReviewUseCaseResponse.review.createdAt,
-                updatedAt: getReviewUseCaseResponse.review.updatedAt,
-            },
-            time: getReviewUseCaseResponse.review.walk.time.value,
-            distance: getReviewUseCaseResponse.review.walk.distance.value,
-            // walkwayImage: getReviewUseCaseResponse.review.walk.walkway.image.value,
-            address: getReviewUseCaseResponse.review.walk.walkway.address.value,
-            // images: getReviewUseCaseResponse.review.images.value,
-            like,
-        };
-        return {
-            review,
-        };
-    }
-
-    @Patch('/:reviewId')
-    @HttpCode(StatusCodes.NO_CONTENT)
-    @ApiResponse({
-        type: CommonResponse,
-    })
-    async update(
-        @Body() request: UpdateReviewRequest,
-        @Param('reviewId') reviewId: string,
-    ): Promise<CommonResponse> {
-        // TODO: 차후 UseCase 생성 시 추가
-        return {
-            code: StatusCodes.NO_CONTENT,
-            responseMessage: 'SUCCESS TO UPDATE REVIEW',
-        }        
-    }
-
     @Get('/feeds')
     @ApiOkResponse({
         type: GetAllFeedReseponse,
     })
     @HttpCode(StatusCodes.OK)
     @ApiOperation({
-        description: 'Feed 목록 (feed 페이지에서의 리뷰 정보 목록)를 반환'
+        description: 'Feed 목록 (feed 페이지에서의 리뷰 정보 목록)를 반환 / '
         + 'userId를 보낼 경우: 해당하는 user의 리뷰 리스트 반환 (마이페이지>작성한 산책로) / '
         + '보내지 않을 경우: 전체 피드 리스트 반환 (피드 페이지)'
     })
@@ -256,34 +193,102 @@ export class ReviewController {
             throw new HttpException('FAIL TO GET ALL FEED', StatusCodes.INTERNAL_SERVER_ERROR)
         }
 
-        const reviews = _.filter(
-            _.map(getAllReviewUseCaseResponse.reviews, 
-                (review) => ({
-                    review: {
-                        id: review.id,
-                        title: review.title.value,
-                        vehicle: review.vehicle,
-                        star: review.star.value,
-                        content: review.content.value,
-                        userImage: review.walk.user.image.value,
-                        userName: review.walk.user.name.value,
-                        walkwayId: review.walk.walkway.id,
-                        walkwayTitle: review.walk.walkway.title.value,
-                        createdAt: review.createdAt,
-                        updatedAt: review.updatedAt,
-                    },
-                    time: review.walk.time.value,
-                    distance: review.walk.distance.value,
-                    // walkwayImage: review.walk.walkway.image.value,
-                    address: review.walk.walkway.address.value,
-                    // images: review.images.value,
-                })
-            )
-        );
+        let reviews = []
+        for (const review of getAllReviewUseCaseResponse.reviews) {
+            const tmp = {
+                review: {
+                    id: review.id,
+                    title: review.title.value,
+                    vehicle: review.vehicle,
+                    star: review.star.value,
+                    content: review.content.value,
+                    userImage: review.walk.user.image.value,
+                    userId: review.walk.user.id,
+                    userName: review.walk.user.name.value,
+                    walkwayId: review.walk.walkway.id,
+                    walkwayTitle: review.walk.walkway.title.value,
+                    createdAt: review.createdAt,
+                    updatedAt: review.updatedAt,
+                },
+                time: review.walk.time.value,
+                distance: review.walk.distance.value,
+                // walkwayImage: review.walk.walkway.image.value,
+                address: review.walk.walkway.address.value,
+                // images: review.images.value,
+                like: await is_like_exist(review, userId, this.getUserUseCase, this.getLikeUseCase),
+            }
+            reviews.push(tmp);
+        }
 
         return {
             reviews,
         }
+    }
+
+    @Get('/:reviewId')
+    @ApiOkResponse({
+        type: GetReviewResponse,
+    })
+    @ApiOperation({
+        summary: '개별 리뷰 정보 가져오기',
+        description: '피드>산책로게시물 페이지에 보여질 리뷰 정보 get / userId는 좋아요 여부를 알기 위함. / ' 
+        + 'userId가 주어지지 않으면 like는 false로 리턴'
+    })
+    async getReview(
+        @Param('reviewId') reviewId: string,
+        @Query('userId') userId: string,
+    ): Promise<GetReviewResponse> {
+        const getReviewUseCaseResponse = await this.getReviewUseCase.execute({
+            id: reviewId,
+        });
+        if (getReviewUseCaseResponse.code === GetReviewUseCaseCodes.NO_EXIST_REVIEW)
+            throw new HttpException('NO EXIST REVIEW', StatusCodes.NOT_FOUND);
+
+        if (getReviewUseCaseResponse.code !== GetReviewUseCaseCodes.SUCCESS) {
+            throw new HttpException('FAIL TO FIND REVIEW', StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+
+        const review: FeedDto = {
+            review: {
+                id: getReviewUseCaseResponse.review.id,
+                title: getReviewUseCaseResponse.review.title.value,
+                vehicle: getReviewUseCaseResponse.review.vehicle,
+                star: getReviewUseCaseResponse.review.star.value,
+                content: getReviewUseCaseResponse.review.content.value,
+                userImage: getReviewUseCaseResponse.review.walk.user.image.value,
+                userId: getReviewUseCaseResponse.review.walk.user.id,
+                userName: getReviewUseCaseResponse.review.walk.user.name.value,
+                walkwayId: getReviewUseCaseResponse.review.walk.walkway.id,
+                walkwayTitle: getReviewUseCaseResponse.review.walk.walkway.title.value,
+                createdAt: getReviewUseCaseResponse.review.createdAt,
+                updatedAt: getReviewUseCaseResponse.review.updatedAt,
+            },
+            time: getReviewUseCaseResponse.review.walk.time.value,
+            distance: getReviewUseCaseResponse.review.walk.distance.value,
+            // walkwayImage: getReviewUseCaseResponse.review.walk.walkway.image.value,
+            address: getReviewUseCaseResponse.review.walk.walkway.address.value,
+            // images: getReviewUseCaseResponse.review.images.value,
+            like: await is_like_exist(getReviewUseCaseResponse.review, userId, this.getUserUseCase, this.getLikeUseCase),
+        };
+        return {
+            review,
+        };
+    }
+
+    @Patch('/:reviewId')
+    @HttpCode(StatusCodes.NO_CONTENT)
+    @ApiResponse({
+        type: CommonResponse,
+    })
+    async update(
+        @Body() request: UpdateReviewRequest,
+        @Param('reviewId') reviewId: string,
+    ): Promise<CommonResponse> {
+        // TODO: 차후 UseCase 생성 시 추가
+        return {
+            code: StatusCodes.NO_CONTENT,
+            responseMessage: 'SUCCESS TO UPDATE REVIEW',
+        }        
     }
 
     @Delete('/:reviewId')
