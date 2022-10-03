b96e0a6cb6c1638eb0c2cfe74ea87892388961b1
SeoSiun <siun0816@hanyang.ac.kr>
2022-10-03 21:24:10 +0900

    UserRefreshToken.ts에서 들여쓰기 수정

--- src/user/domain/User/UserRefreshToken.ts
+++ src/user/domain/User/UserRefreshToken.ts
@@ -4,25 +4,25 @@ import { ValueObject } from '../../../common/domain/ValueObject';
 import { Result } from '../../../common/presentationals/Result';
 
 interface UserRefreshTokenProps {
-  value: string;
+    value: string;
 }
 
 export const USER_REFRESHTOKEN_SHOULD_NOT_BE_EMPTY = 'User refresh token should not be empty.';
 
 export class UserRefreshToken extends ValueObject<UserRefreshTokenProps> {
-	private constructor(props: UserRefreshTokenProps) {
-	  super(props);
-	}
+    private constructor(props: UserRefreshTokenProps) {
+      super(props);
+    }
 
-	static create(userRefreshTokenString: string): Result<UserRefreshToken> {
-		if (_.isEmpty(userRefreshTokenString)) {
-			return Result.fail(USER_REFRESHTOKEN_SHOULD_NOT_BE_EMPTY);
-		}
+    static create(userRefreshTokenString: string): Result<UserRefreshToken> {
+        if (_.isEmpty(userRefreshTokenString)) {
+            return Result.fail(USER_REFRESHTOKEN_SHOULD_NOT_BE_EMPTY);
+        }
 
-		return Result.ok(new UserRefreshToken({ value: userRefreshTokenString }));
-	}
+        return Result.ok(new UserRefreshToken({ value: userRefreshTokenString }));
+    }
 
-	get value(): string {
-		return this.props.value;
-	}
+    get value(): string {
+        return this.props.value;
+    }
 }
