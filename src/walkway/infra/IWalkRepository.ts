3434126e5267049afbc7903f89a648634297970a
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2022-10-03 23:05:12 +0900

    walk에서 review 제거 (#122)
    * walk에서 review 제거
    
    - walk 도메인, 엔티티에서 review제거.
          - toEntity, toDomain도 수정해줌.
    
    - getAllWalkUseCase request에서 option 빼고, user만 받도록 수정.
        - walkwayRepository로 오타난 부분 수정해줌.
        - findAll에서도 userId에 해당하는 유저가 만든 walk를 가져오도록 함.
    
    - 아직 리뷰가 작성되지 않은 walk 목록을 리턴해야하는 경우 (option이 USER_INFO인 경우) 해당 유저의 review 목록을 가져와서 review의 walkId와 일치하는 walk는 필터링해줌.
    
    * 리뷰반영 (세미콜론 추가)

--- src/walkway/infra/IWalkRepository.ts
+++ src/walkway/infra/IWalkRepository.ts
@@ -1,9 +1,9 @@
-import { IGetAllWalkUseCaseRequest } from '../application/GetAllWalkUseCase/dto/GetAllWalkUseCaseRequest';
+import { User } from '../../user/domain/User/User';
 import { Walk } from '../domain/Walk/Walk';
 
 export const WALK_REPOSITORY = Symbol('WALK_REPOSITORY');
 
 export interface IWalkRepository {
-    findAll(request: IGetAllWalkUseCaseRequest): Promise<Walk[]>;
+    findAll(user: User): Promise<Walk[]>;
     save(walk: Walk): Promise<boolean>;
 }
